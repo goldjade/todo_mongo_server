5fabe2ac71576faa738794c725ea71a38a0ce7d6
choi <bcaca0831@gmail.com>
2023-01-10 17:31:14 +0900

    서버연동 및 로딩창 삽입

--- model/TodoModel.js
+++ model/TodoModel.js
@@ -1,11 +1,21 @@
+/** @format */
+
 const mongoose = require('mongoose');
 const todoSchema = new mongoose.Schema(
   {
     id: Number,
     title: String,
     completed: Boolean,
+    //objectId 를 통해서 다른 model에 접근
+
+    author: {
+      //여기에 스키마 하나 더 생성한다고 생각하면됨.
+      //User modeldml의 레퍼런스를 참고함
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
   },
   { collection: 'todos' }
 );
-const Todo = mongoose.model("Todo",todoSchema)
-module.exports = {Todo}
+const Todo = mongoose.model('Todo', todoSchema);
+module.exports = { Todo };
---
+++ model/UserModel.js
@@ -0,0 +1,13 @@
+/** @format */
+
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema(
+  {
+    email: String,
+    displayName: String,
+    uid: String,
+  },
+  { collection: 'users' }
+);
+const User = mongoose.model('User', userSchema);
+module.exports = { User };
